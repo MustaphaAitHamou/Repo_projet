9a36b74c6e622e9f9590e8f00772e39722347a3b
Mustapha Ait-Hamou <mustaphaithamou@gmail.com>
2025-06-23 14:13:08 +0200

    test(e2e): add Cypress config

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,9 +1,13 @@
+// frontend/cypress.config.js
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+
+    // accepte *.cy.js OU *.spec.js partout sous cypress/
+    specPattern: 'cypress/**/*.{cy,spec}.js',
+
     supportFile: false,
-    specPattern: 'cypress/e2e/**/*.cy.js',   // tes specs déplacées ici
   },
 });
